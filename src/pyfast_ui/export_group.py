3edbd47c6b9c2dd6f59aa009146565c145dc8097
matkrin <matkrin@protonmail.com>
2025-02-13 18:41:24 +0100

    Add support for exporting single frames as .txt files
    The txt files contain an ASCII matrix of the image data values which can
    also be consumed by Gwyddion.

--- src/pyfast_ui/export_group.py
+++ src/pyfast_ui/export_group.py
@@ -78,7 +78,7 @@ class ExportGroup(QGroupBox):
 
         frame_export_format_lbl = QLabel("Frame export format")
         self._frame_export_format = QComboBox()
-        self._frame_export_format.addItems(["png", "jpg", "bmp", "gwy"])
+        self._frame_export_format.addItems(["png", "jpg", "bmp", "gwy", "txt"])
         self._frame_export_format.setCurrentText(frame_export_format)
         frame_export_format_layout = QHBoxLayout()
         frame_export_format_layout.addWidget(frame_export_format_lbl)
